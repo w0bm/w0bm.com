510cd4333f1207cefec08f405ce2d4131d6ebadd
belst <w0bm@bel.st>
2015-09-11 18:50:11 +0200

    simplifies markup

--- resources/views/songindex.blade.php
+++ resources/views/songindex.blade.php
@@ -1,40 +1,32 @@
 @extends('layout')
 @section('content')
-    <div class="row">
-        <div class="page-header">
-            <h1>Songindex</h1>
-        </div>
+    <div class="page-header">
+        <h1>Songindex</h1>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <table class="table table-hover table-condensed table-striped">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Interpret</th>
-                    <th>Songtitle</th>
-                    <th>Image Source</th>
-                    <th>Category</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($videos as $video)
-                    <tr>
-                        <td><a href="{{$video->id}}">{{$video->id}}</a></td>
-                        <td>{{$video->interpret or ''}}</td>
-                        <td>{{$video->songittle or ''}}</td>
-                        <td>{{$video->imgsource or ''}}</td>
-                        <td><a href="category/{{$video->category->id}}">{{$video->category->name}}</a></td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <table class="table table-hover table-condensed table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Interpret</th>
+            <th>Songtitle</th>
+            <th>Image Source</th>
+            <th>Category</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($videos as $video)
+            <tr>
+                <td><a href="{{$video->id}}">{{$video->id}}</a></td>
+                <td>{{$video->interpret or ''}}</td>
+                <td>{{$video->songittle or ''}}</td>
+                <td>{{$video->imgsource or ''}}</td>
+                <td><a href="category/{{$video->category->id}}">{{$video->category->name}}</a></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 
-    <div class="row">
-        <div class="col-md-12 text-right">
-            {!! $videos->render() !!}
-        </div>
+    <div class="pull-right">
+        {!! $videos->render() !!}
     </div>
 @endsection
