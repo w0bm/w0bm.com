bd7decb63041864f9993ddcfac180d240c1d8c54
belst <w0bm@bel.st>
2015-09-12 10:36:46 +0200

    messagebag errors in flash view

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -35,7 +35,7 @@
             </div>
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-default">Register</button>
+                    <button type="submit" class="btn btn-primary">Register</button>
                 </div>
             </div>
         </form>
