3347164f0f447fedc7af87dea0d5b106fdfc1d52
belst <w0bm@bel.st>
2015-09-13 13:36:53 +0200

    adds category index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return view('categories', ['categories' => Category::all()]);
     }
 
     /**
