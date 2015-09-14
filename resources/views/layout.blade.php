9f9d9fc99c637402486ae65c4373f588ef0aa849
belst <w0bm@bel.st>
2015-09-14 21:09:38 +0200

    testing slidedown

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -175,7 +175,7 @@
 
     (function ($) {
         $('.alert').each(function() {
-            $(this).delay(3000).slideDown(300);
+            $(this).delay(3000).slideUp(300);
         });
     })(jQuery);
 
