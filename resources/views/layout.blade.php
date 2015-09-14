604fcecfe7aa657af7f43edb1599b8f724ee1692
belst <w0bm@bel.st>
2015-09-14 19:52:25 +0200

    doesnt resize comment box when it has content

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -85,7 +85,7 @@
             height: 2.7em;
             resize: vertical;
         }
-        .comments textarea:active, .comments textarea:focus {
+        .comments textarea:active, .comments textarea:focus, .comments textarea:not(:empty) {
             height: 8em;
         }
     </style>
