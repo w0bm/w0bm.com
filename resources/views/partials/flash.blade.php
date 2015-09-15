d9847c29b69b2f4783dd45b6cac9d8c5a611b5b1
belst <w0bm@bel.st>
2015-09-15 10:30:24 +0200

    changes flash position

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,34 +1,36 @@
-<div class="container flashcontainer">
-    @if($errors->count() > 0)
-        @foreach($errors->all() as $error)
+<div class="flashcontainer">
+    <div class="container">
+        @if($errors->count() > 0)
+            @foreach($errors->all() as $error)
+                <div class="alert alert-danger alert-dismissable" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    {{ $error }}
+                </div>
+            @endforeach
+        @endif
+        @if(Session::has('error'))
             <div class="alert alert-danger alert-dismissable" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                {{ $error }}
+                {{ session('error') }}
             </div>
-        @endforeach
-    @endif
-    @if(Session::has('error'))
-        <div class="alert alert-danger alert-dismissable" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            {{ session('error') }}
-        </div>
-    @endif
-    @if(Session::has('success'))
-        <div class="alert alert-success alert-dismissable" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            {{ session('success') }}
-        </div>
-    @endif
-    @if(Session::has('info'))
-        <div class="alert alert-info alert-dismissable" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            {{ session('info') }}
-        </div>
-    @endif
-    @if(Session::has('warning'))
-        <div class="alert alert-warning alert-dismissable" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            {{ session('warning') }}
-        </div>
-    @endif
+        @endif
+        @if(Session::has('success'))
+            <div class="alert alert-success alert-dismissable" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                {{ session('success') }}
+            </div>
+        @endif
+        @if(Session::has('info'))
+            <div class="alert alert-info alert-dismissable" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                {{ session('info') }}
+            </div>
+        @endif
+        @if(Session::has('warning'))
+            <div class="alert alert-warning alert-dismissable" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                {{ session('warning') }}
+            </div>
+        @endif
+    </div>
 </div>
