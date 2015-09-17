2fd890d503e156257689acfac5093054fd0c0cfb
belst <w0bm@bel.st>
2015-09-17 11:15:42 +0200

    fixes delete comment route

--- resources/views/partials/comments.blade.php
+++ resources/views/partials/comments.blade.php
@@ -12,7 +12,7 @@
                     <div class="panel-footer">by <a href="/user/{{$comment->user->username}}">{{$comment->user->username}}</a> <small>{{$comment->created_at->diffForHumans()}}</small>
                     @if(auth()->check())
                         @if(auth()->user()->can('delete_comment'))
-                            <br><span class="label label-danger"><small><a href="{{url('comment/' . $comment->id . '/delete')}}">Delete</a></small></span>
+                            <br><a class="btn btn-danger" href="{{url('comment/' . $comment->id . '/delete')}}">Delete</a>
                         @endif
                     @endif
                     </div>
