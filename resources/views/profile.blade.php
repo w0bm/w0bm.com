4b9e2907a3a037740f51f7a67a19e14938e15ea0
Joshua <git@sky-e.de>
2015-09-17 20:29:07 +0200

    changes layout

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,8 +2,7 @@
 @section('content')
 
     <div class="page-header">
-        <h1>{{$user->username}}</h1>
-        <span>Uploads: {{ $user->videos()->count() }}</span>
+        <h1>{{$user->username}} <small>Uploads: {{ $user->videos()->count() }}</small></h1>
     </div>
 
 
