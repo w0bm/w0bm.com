b927bc69258e26c16553dd7d9e7253a5e6b2ae57
belst <w0bm@bel.st>
2015-09-19 20:31:53 +0200

    adds video info to page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -261,7 +261,9 @@
     });
 
     $(function() {
-        $('[data-toggle="popover"]').popover();
+        $('[data-toggle="popover"]').popover({
+            html: true
+        });
     });
         
     
