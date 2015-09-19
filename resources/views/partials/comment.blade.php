e340098f3f35501898741fb22ec0173ec3809f5f
belst <w0bm@bel.st>
2015-09-19 22:22:59 +0200

    adds possibility to restore comments

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -4,9 +4,10 @@
     </div>
     <div class="panel-footer">by <a href="/user/{{$comment->user->username}}">{{$comment->user->username}}</a> <small>{{$comment->created_at->diffForHumans()}}</small>
         @if($mod)
-            <a data-confirm="Do you really want to delete that comment?" class="btn btn-danger" href="{{url('comment/' . $comment->id . '/delete')}}">Delete</a>
             @if($del)
                 <a href="{{url('comment/' . $comment->id . '/restore')}}" class="btn btn-success">Restore</a>
+            @else
+                <a data-confirm="Do you really want to delete that comment?" class="btn btn-danger" href="{{url('comment/' . $comment->id . '/delete')}}">Delete</a>
             @endif
         @endif
     </div>
