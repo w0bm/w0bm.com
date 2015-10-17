c3a34245eb4fb7bcdab2f5be86d9cd21a41f92d0
belst <belstgut@sky-e.de>
2015-10-17 23:04:49 +0200

    adds read column to messages table

---
+++ database/migrations/2015_10_17_210135_add_read_field_to_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddsReadFieldToMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function(Blueprint $table) {
+            $table->timestamp('read')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function(Blueprint $table) {
+            $table->dropColumn('read');
+        });
+    }
+}
