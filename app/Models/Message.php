959358ad98878111a2c8a614fd3fdcf7c168b64d
belst <belstgut@sky-e.de>
2015-10-17 23:17:54 +0200

    Adds sendmessage function

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model {
@@ -16,7 +17,27 @@ class Message extends Model {
         return $this->belongsTo(User::class, 'to');
     }
 
-    public static function send($from, $to, $content) {
+    public static function send($from, $to, $subject, $content) {
+        if(is_object($from)) $from = $from->id;
+        if(is_object($to)) $to = $to->id;
 
+        if(empty($subject)) return 'Subject must not be empty';
+        if(empty($content)) return 'Content must not be empty';
+
+        try {
+            User::findOrFail($from);
+            User::findOrFail($to);
+        } catch (ModelNotFoundException $e) {
+            return $e->getMessage();
+        }
+
+        $message = new static();
+        $message->from    = $from;
+        $message->to      = $to;
+        $message->subject = $subject;
+        $message->content = $content;
+        $message->save();
+
+        return $message;
     }
 }
