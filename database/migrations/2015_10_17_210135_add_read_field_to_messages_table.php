807218bb2765d2e62561aed45d5539bc7458a741
belst <belstgut@sky-e.de>
2015-10-18 15:47:43 +0200

    fixes typo

--- database/migrations/2015_10_17_210135_add_read_field_to_messages_table.php
+++ database/migrations/2015_10_17_210135_add_read_field_to_messages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddsReadFieldToMessagesTable extends Migration
+class AddReadFieldToMessagesTable extends Migration
 {
     /**
      * Run the migrations.
