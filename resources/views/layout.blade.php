f505373d9ab7742cc1bcef678e1077c7b9e2ef21
Joshua <git@sky-e.de>
2015-10-19 20:59:03 +0200

    fixes bg color again

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
     <link href="//fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <style>
-        .body { background: #161618; }
+        body { background: #161618; }
         .flashcontainer {
             position:absolute;
             top:20px;
