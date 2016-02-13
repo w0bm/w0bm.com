4c5e97676be84c86a5c58eda2511a554e33e5b7a
jkhsjdhjs <jkhsjdhjs@users.noreply.github.com>
2016-02-13 17:28:25 +0100

    temporary fix 'scrolling not working on other pages'
    fixes the bug that scrolling isn't working on profile and other pages.
    however, it should be temporary and removed when a better solution is found.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -199,15 +199,19 @@
         }
     })(jQuery);
     
-    $('.wrapper > div').on('DOMMouseScroll mousewheel', function(e) {
-    	if(e.deltaY < 0 && $('#next').css('visibility') != 'hidden') {
-    		document.getElementById('next').click();
-    	}
-    	else if($('#prev').css('visibility') != 'hidden') {
-    		document.getElementById('prev').click();
-    	}
-    	return false;
-    });
+    //temporary fix for scrolling not working on other pages
+    var regex = /w0bm.com\/(?:.+\/)?(\d+)/i;
+    if(regex.test(window.location.href) && $('video').length) {
+        $('.wrapper > div').on('DOMMouseScroll mousewheel', function(e) {
+        	if(e.deltaY < 0 && $('#next').css('visibility') != 'hidden') {
+        		document.getElementById('next').click();
+        	}
+        	else if($('#prev').css('visibility') != 'hidden') {
+        		document.getElementById('prev').click();
+        	}
+        	return false;
+        });
+    }
 
     (function ($) {
         $.ajaxSetup({
