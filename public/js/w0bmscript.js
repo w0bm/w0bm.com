4b6320f6e446f5d10ef2334912ad4d6a044855bf
jkhsjdhjs <jkhsjdhjs@users.noreply.github.com>
2016-06-01 18:38:23 +0200

    Update w0bmscript.js (#58)
    updated w0bmscript regex

--- public/js/w0bmscript.js
+++ public/js/w0bmscript.js
@@ -46,7 +46,7 @@ function flash(type, message) {
     }
 
     //temporary fix for scrolling not working on other pages
-    var regex = /w0bm.com\/(?:.+\/)?(\d+)/i;
+    var regex = /\..+\/(?:.+\/)?(\d+)/i;
     if(regex.test(window.location.href) && $('video').length) {
         $('html').on('keydown', function(e) {
             if(e.defaultPrevented || e.target.nodeName.match(/\b(input|textarea)\b/i)) {
