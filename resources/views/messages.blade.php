e585afa72ba95fa92af603c91548de05f998562f
jkhsjdhjs <leon.moeller@yahoo.de>
2016-06-09 00:56:56 +0200

    timezone correction for tooltips

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -5,7 +5,7 @@
     </div>
 	@foreach(auth()->user()->messagesRecv->reverse() as $message)
         <div class="message panel panel-default">
-            <div class="panel-heading">{!! str_replace('</h3>', '</h3><span data-toggle="tooltip" data-container=".tooltip-container" data-placement="top" title="'.$message->created_at->format("d.m.Y H:i").'">'.$message->created_at->diffForHumans().'</span></div><div class="panel-body">',$message->content) !!}</div>
+            <div class="panel-heading">{!! str_replace('</h3>', '</h3><span data-toggle="tooltip" data-container=".tooltip-container" data-placement="top" title="'.$message->created_at->timezone('Europe/Berlin')->addHour(1)->format("d.m.Y H:i").'">'.$message->created_at->diffForHumans().'</span></div><div class="panel-body">',$message->content) !!}</div>
         </div>
         <div class="tooltip-container"></div>
         <br>
