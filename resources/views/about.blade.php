d0b76fc2c36d1cd7dea110821c65c1e9e1264cc8
Joshua <git@sky-e.de>
2016-06-11 17:31:19 +0200

    replaces text-center with text-align: center

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -32,7 +32,7 @@
 <br>
 <br>
 <nav class="navbar navbar-default navbar-fixed-bottom">
-  <div class="container text-center">
+  <div class="container" style="text-align: center">
     <a href="/contact">Contact</a> | <a href="/privacy">Privacy Policy</a> | <a href="/donate">Donate</a>
 <p>Inspired by <a href="http://z0r.de">z0r.de</a></p>
   </div>
