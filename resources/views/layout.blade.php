df0fb319e7ae63996aba31ed9bde19a6845195db
Joshua <git@sky-e.de>
2016-06-15 16:55:52 +0200

    allows video uploader to modify video

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,7 +20,7 @@
 <body>
 @if(auth()->check())
     @include('partials.filterselect')
-    @if(auth()->user()->can('edit_video'))
+    @if(isset($video) && (auth()->user()->can('edit_video') || auth()->user()->id == $video->id))
         @include('partials.frontendedit')
     @endif
 @endif
