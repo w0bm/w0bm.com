027d703af874be7c6f69196c4b2e1d0dd2a8aba2
jkhsjdhjs <leon.moeller@yahoo.de>
2016-06-24 00:53:54 +0200

    adds profile header partial and empty comment view

---
+++ resources/views/comments.blade.php
@@ -0,0 +1,12 @@
+@extends('profilelayout')
+@section('content')
+@include('partials.profileheader')
+    <div class="row">
+        <div class="col-md" id="list">
+            <div class="spinner">
+                <div class="cube1"></div>
+                <div class="cube2"></div>
+            </div>
+        </div>
+    </div>
+@endsection
