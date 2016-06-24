91b739b698cb4e56efd0587ebcb81d0c98468f56
jkhsjdhjs <leon.moeller@yahoo.de>
2016-06-24 16:35:32 +0200

    removes "

--- resources/views/partials/comlist.php
+++ resources/views/partials/comlist.php
@@ -2,7 +2,7 @@
             {{#each data as |value key|}}
             <div class="panel panel-default">
                 <div class="panel-body" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;" data-index="{{key}}" data-id="{{id}}">{{{rendered_view}}}</div>
-                <div class="panel-footer">"Video: <a href="/{{video_id}}">/{{video_id}}</a> | <time class="timeago" datetime="{{created_at}}" title="{{created_at}}">{{created_at}}</time></div>
+                <div class="panel-footer">Video: <a href="/{{video_id}}">/{{video_id}}</a> | <time class="timeago" datetime="{{created_at}}" title="{{created_at}}">{{created_at}}</time></div>
             </div>
             {{else}}
             <div class="list-group-item">No comments</div>
