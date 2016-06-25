475a29534f9675fcdf8b5cf3c6e0bb9b0c64a807
jkhsjdhjs <leon.moeller@yahoo.de>
2016-06-25 02:27:30 +0200

    popovers stay visible on hovering

--- public/js/w0bmscript.js
+++ public/js/w0bmscript.js
@@ -264,7 +264,23 @@ $('#categories').imagesLoaded(function () {
 
 $(function() {
     $('[data-toggle="popover"]').popover({
-        html: true
+        html: true,
+        trigger: 'manual',
+        container: $(this).attr('id'),
+        placement: 'top',
+    }).on('mouseenter', function () {
+        var _this = this;
+        $(this).popover('show');
+        $(this).siblings('.popover').on('mouseleave', function () {
+            $(_this).popover('hide');
+        });
+    }).on('mouseleave', function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$('.popover:hover').length) {
+                $(_this).popover('hide')
+            }
+        }, 100);
     });
 });
 
