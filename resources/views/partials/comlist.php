5b08baea729e3822dab9863e2d67c31b93363b53
jkhsjdhjs <leon.moeller@yahoo.de>
2016-06-26 12:22:50 +0200

    changes useless ellipsis to break-word

--- resources/views/partials/comlist.php
+++ resources/views/partials/comlist.php
@@ -1,7 +1,7 @@
 <script id="comlist" type="text/x-handlebars-template">
             {{#each data as |value key|}}
             <div class="panel panel-default">
-                <div class="panel-body" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;" data-index="{{key}}" data-id="{{id}}">{{{rendered_view}}}</div>
+                <div class="panel-body" style="word-wrap: break-word;" data-index="{{key}}" data-id="{{id}}">{{{rendered_view}}}</div>
                 <div class="panel-footer">Video: <a href="/{{video_id}}">/{{video_id}}</a> | <time class="timeago" datetime="{{created_at}}" title="{{created_at}}">{{created_at}}</time></div>
             </div>
             {{else}}
