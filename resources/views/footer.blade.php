36f889676ea0efdf8da74eba757b9667070c096c
Robo <sirx@waifu.club>
2016-07-05 19:31:59 +0200

    changed the whole login stuff, looks now way better. Also changed the donation page.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar-fixed-bottom">
-  <div class="container pull-right" style="text-align: right;">
+  <div class="container futter">
     <a href="/contact">Contact</a> | <a href="/privacy">Privacy</a> | <a href="/donate">Donate</a>
 <p>Inspired by <a href="http://z0r.de">z0r.de</a></p>
   </div>
