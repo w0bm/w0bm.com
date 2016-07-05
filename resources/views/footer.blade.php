f5276b8b30a5b068c4e6f1c6daaee7bd65d48fcc
Robo <sirx@waifu.club>
2016-07-05 14:41:18 +0200

    better looking footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-default navbar-fixed-bottom">
-  <div class="container" style="text-align: center">
-    <a href="/contact">Contact</a> | <a href="/privacy">Privacy Policy</a> | <a href="/donate">Donate</a>
+<nav class="navbar-fixed-bottom">
+  <div class="container pull-right" style="text-align: right;">
+    <a href="/contact">Contact</a> | <a href="/privacy">Privacy</a> | <a href="/donate">Donate</a>
 <p>Inspired by <a href="http://z0r.de">z0r.de</a></p>
   </div>
 </nav>
