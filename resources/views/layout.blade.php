6f3b1a4fb6350695bbf0939d10b67ba5bba865b3
jkhsjdhjs <leon.moeller@yahoo.de>
2016-08-19 22:30:10 +0200

    bump stylesheet version

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
     <link href="//fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link href="/css/video-js.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/w0bmcustom.css?v=1.0.9">
+    <link rel="stylesheet" href="/css/w0bmcustom.css?v=1.0.10">
 </head>
 <body>
 @if(auth()->check())
