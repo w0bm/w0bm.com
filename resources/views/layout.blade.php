aa47fb911035b25d11933238282fd4c634ca2fc1
belst <w0bm@bel.st>
2016-08-20 22:00:34 +0200

    adding ids to tab

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="_token" content="{{csrf_token()}}">
     <link rel="icon" href="/favicon.png">
-    <title>w0bm.com – WebMs with sound!</title>
+    <title>@if(isset($video)){{ $video->id }}@endif – w0bm.com</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}">
     <link rel="favicon" type="image/ico" href="favicon.ico" />
