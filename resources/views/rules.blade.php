d82b763ead06ac28bfd6cf8857635e53ea19f697
belst <w0bm@bel.st>
2016-08-22 02:59:18 +0200

    updating rules to proper english. alucard™

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -13,22 +13,17 @@
 <h5>Uploading</h5>
 <ol>
 	<li>Everything you upload <strong style="color:#1FB2B0;">must</strong> be acceptable with Dutch law</li>
-	<li>Absolutely no childporn in any ways! And no, loli content is not childporn, stop talking shit like this, if you can't acceppt those 5 loli videos get the fuck out!</li>
-	<li>No shocking content, I mean mostly Gore with this, this site is friendly and shouldn't scare people in any ways, with exception of weird porn, this includes scat, puke and all the weird japanese fetish videos, they are all welcome here!</li>	
+	<li>Absolutely no child pornography in any way! Loli content is excluded from this, so shut the fuck up; if you can't accept those 5 loli videos get the fuck out!</li>
+	<li>No shocking content, I mean mostly Gore with this, this site is friendly and shouldn't scare people in any ways, with the exception of weird porn. This includes scat, puke, and all the weird japanese fetish videos. They are all welcome here!</li>	
 	<li>Reposting is not a crime! But in most cases it can be prevented with the use of the <a href="https://w0bm.com/songindex">search form</a>.
 	<li>Put your uploads in the correct category, if you have a video that doesn't fit in any of the categories always put it in the Misc category</li.>
 	<li>The focus is on videos you can easily watch 200 times a day over and over so upload only catchy shit! For example: <a href="/39">/39</a></li>
-	<li>The Interpret and Songtitle field is only for the Song and Artist information, anything else in there will be overwritten and when this happens too often your account will be banned for at least 2 months!</li>
+	<li>The Artist and Songtitle field is only for the Artist and Song information respectively, anything else in there will be overwritten and when this happens too often your account will be banned for at least 2 months!</li>
 </ol>
 
-<h5>Suggestion for every users</h5>
-<ul style="text-align:center;">
+<h5>Suggestion for every user</h5>
+<ul style="text-align:left;">
 	<li>Before you should think about uploading videos to w0bm you should have seen every single video here, we don't want to delete videos because you are too lazy to watch em all! Everytime you repost a ID dies and we don't want that. Like we said it's not a crime but if it happens too often you can expect a ban for not reading and understanding our rules</li>
 </ul>
-
-<center>
-<h5 style="color:red;">Those who are unable to read follow and comply with at least the General rules should expect a lengthy ban.</h5>
-<h6 style="color:#1FB2B0;">+++These rules can change at any time and without any further notice, if you can't follow these rules you are not welcome here!+++</h6>
-</center>
 @include('footer')
 @endsection
