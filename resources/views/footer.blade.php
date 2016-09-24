f08f4bff6a11d8795931426961e0ba68df4115c5
sirx <admin@w0bm.com>
2016-09-24 18:10:36 +0200

    worldmap

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar-fixed-bottom">
   <div class="container futter">
-    <a href="/rules">Rules</a> | <a href="/contact">Contact</a> | <a href="https://twitter.com/w0bm_com">Twitter</a> | <a href="/privacy">Privacy</a> | <a href="/transparency">Transparency</a> | <a href="/donate">Donate</a>
+    <a href="/rules">Rules</a> | <a href="/contact">Contact</a> | <a href="https://twitter.com/w0bm_com">Twitter</a> | <a href="/privacy">Privacy</a> | <a href="/transparency">Transparency</a> | <a href="/donate">Donate</a> | <a href="/map">Map</a>
 <p>Inspired by <a href="http://z0r.de">z0r.de</a></p>
   </div>
 </nav>
