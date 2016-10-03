2cd90c639f585b73ca95fe9bc3ae646e1b4237e8
jkhsjdhjs <leon.moeller@yahoo.de>
2016-10-03 23:18:02 +0200

    tagging style

--- resources/views/partials/tags.blade.php
+++ resources/views/partials/tags.blade.php
@@ -1,14 +1,14 @@
 <aside class="tags hidden-xs">
     @if(Auth::check())
-        <div class="input-group">
+        <div class="input-group" style="margin-top: 2px;">
             {{--<select multiple name="tags[]" data-role="tagsinput" class="form-control"></select>--}}
             <input id="tags" type="text" class="form-control" placeholder="Input tags…" name="tags" data-role="tagsinput">
             <span class="input-group-btn">
-                <button href="/{{$video->id}}/tag" id="submittags" type="submit" class="form-control">Submit</button>
+                <button href="/{{$video->id}}/tag" id="submittags" type="submit" class="form-control btn-primary">Submit</button>
             </span>
         </div>
     @endif
-    <div class="panel panel-default">
+    <div class="panel panel-default" style="margin-top: 5px;">
         <div id="tag-display" class="panel-body">
             @if(count($video->tags))
                 @foreach($video->tags as $tag)
