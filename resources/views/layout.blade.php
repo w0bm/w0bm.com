761f45d7c6536657568193aa2c62b65ede63785f
sirx <sirx@firemail.cc>
2016-11-19 16:07:47 +0100

    mach piwik wieder rein :( GA ist doof :(

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,16 +17,22 @@
     <link rel="stylesheet" href="/css/bootstrap-tagsinput.css">
     <link rel="stylesheet" href="/css/video-js.min.css">
     <link rel="stylesheet" href="/css/w0bmcustom.css?v=1.0.26">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-87687440-1', 'auto');
-  ga('send', 'pageview');
-
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+    _paq.push(["setDomains", ["*.w0bm.com"]]);
+    _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+	          var u="//stats.w0bm.com/";
+		      _paq.push(['setTrackerUrl', u+'js']);
+		      _paq.push(['setSiteId', '1']);
+		          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'js'; s.parentNode.insertBefore(g,s);
+			    })();
 </script>
+<noscript><p><img src="//stats.w0bm.com/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 </head>
 <body>
 @if(auth()->check())
