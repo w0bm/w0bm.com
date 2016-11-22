ce8a6c7687d319d934304780f9b34b7909351739
Sirx <n0xy@cock.li>
2016-11-22 21:14:08 +0100

    Update rules.blade.php

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -12,7 +12,7 @@
 <h5>Uploading</h5>
 <ol>
 	<li>Everything you upload <strong style="color:#1FB2B0;">must</strong> be acceptable with Dutch law</li>
-	<li>Absolutely no child pornography in any way! Loli content is excluded from this, so shut the fuck up; if you can't accept those 5 loli videos get the fuck out!</li>
+	<li>Absolutely no child pornography in any way! This also includes "modeling" and 3D loli videos. 2D (hentai) is okay.</li>
 	<li>Videos that qualify as Nazi videos need to be marked with the nsfw tag an put into the Mett category</li>
 	<li>Reposting is not a crime! But in most cases it can be prevented with the use of the <a href="https://w0bm.com/songindex">search form</a>.
 	<li>Put your uploads in the correct category, if you have a video that doesn't fit in any of the categories always put it in the Misc category</li.>
