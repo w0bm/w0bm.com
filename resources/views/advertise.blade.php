5cd4f6b3947d34c842975c4dd794d28b49d59e9e
w0bm <admin@w0bm.com>
2017-01-05 08:51:53 +0100

    xD

--- resources/views/advertise.blade.php
+++ resources/views/advertise.blade.php
@@ -5,7 +5,7 @@
 </div>
 <h5>How?</h5>
 <p>Just send me an email to admin@w0bm.com with the things you would like to advertise. I don't care it's basically for the lulz. It can be anything, your YouTube Channel some website a shampoo it's up to you.</p>
-<p>Ads will be up for max 2 days</p>
+<p>Ads will be up for like 2 weeks maybe or longer I don't know. We'll see.</p>
 <p>It's 100% free because with lulz u don't make moneyz</p>
 <p>It's also possible to send in small banners with the exact size of 342x50.</p>
 <p>Example:</p>
