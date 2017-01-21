c01c13132e1542103351beb0ed11ea06389de78f
Sirx <n0xy@cock.li>
2017-01-21 22:14:36 +0100

    Update profilelayout.blade.php

--- resources/views/profilelayout.blade.php
+++ resources/views/profilelayout.blade.php
@@ -64,6 +64,6 @@
 <script src="/js/imagesloaded.pkgd.min.js"></script>
 <script src="/js/handlebars.min.js"></script>
 <script src="/js/jquery.timeago.js"></script>
-<script src="/js/w0bmscript.js?v=1.1.12"></script>
+<script src="/js/w0bmscript.js?v=1.1.13"></script>
 </body>
 </html>
