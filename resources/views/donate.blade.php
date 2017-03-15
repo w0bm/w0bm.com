bd751f692c533a5ae1985d394c4915d837a4292f
w0bm <admin@w0bm.com>
2017-03-15 20:06:54 +0100

    donation page update

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -7,27 +7,11 @@ use \App\Models\Donation;
         <h4>Support us</h4>
     </div>
 <div class="row">
-  <div class="col-xs-6">
-<h5>About: ~</h5>
-<ul>
-<li><p>First of all we as your webm provider want to be as transparent as possible to you, our so loved user, feel assfucked :3</p></li>
-<li><p>w0bm is a slut, a traffic slut, a money slut and a slut in general, you know what I mean.</p></li>
-<li><p>It's getting more difficult to pay the bill with the increasing amount of users, so it's time to reopen donations to collect enough money for the server.</p></li>
-<li><p>We know that people love shit, thats apparently one reason why people are browsing w0bm but that's not what I mean in first place, I am talking about something little you will get for your donation beside an alive and well doing w0bm</p></li>
-</ul>
-<h6>Rewards:</h6>
-<p>We are now on Patreon! If you'd like to support us please take a look at <a href="https://www.patreon.com/w0bm">our Patreon page</a></p>
-<ul>
-<li><p>If you donate 5€ you can choose an icon from <a href="http://fontawesome.io/icons/">fontawesome.io/icons</a></p></li>
-<li><p>If you donate 10€ or more you can choose your own personalized icon.</p></li>
-<li><p>To claim your icon please join our <a href="/irc">IRC</a></p></li>
-  </div>
-  <div class="col-xs-6">
 </div>
 
 <div class="col-md-6">
-	<h5>Overview: ~</h5>
-	<p>Like us? Help to keep it alive.</p>
+	<h5>Progress</h5>
+	<p>You enjoy w0bm? Donate a small amount of money so we can pay the server and keep this website running.</p>
 	<div class="progress" style="width: 100%">
         @if(Donation::getFunds() >= 0)
   		    <div class="progress-bar-success" role="progressbar" aria-valuenow="{{ Donation::getFunds() }}" aria-valuemin="0" aria-valuemax="{{ Donation::$needed }}" style="text-align:center; color:white; width: {{ max(0, min(100, abs(Donation::getPercentage()))) }}%;">
@@ -37,18 +21,19 @@ use \App\Models\Donation;
 		{{ Donation::getFunds() }} €
 		</div>
 	</div>
-	<p>Did you know: BTC is super cute :3</p>
-	<i class="fa fa-btc" style="color: #1FB2B0;" aria-hidden="true"></i> <span style="font-family:monospace;color:#1FB2B0;">1PXqtRtZLYwRFKKks7tipVXeauGSpy3u4z</span>
+	<p>Currently we accept PayPal and Bitcoin</p>
+	<i class="fa fa-btc" style="color: #1FB2B0;" aria-hidden="true"></i> <span style="font-family:monospace;color:#1FB2B0;">1PXqtRtZLYwRFKKks7tipVXeauGSpy3u4z</span> <span><b>[ PREFERED ]</b></span>
 	<br />
 	<i class="fa fa-paypal" style="color: #1FB2B0;"></i> <span style="font-family:monospace;coloR:#1FB2B0;">paypal@w0bm.com</span>
+	<br />
+	<p>We also opened up a Patreon account if you want to support w0bm every month <a href="https://www.patreon.com/w0bm">click here.</a></p>
+	<p>There is a small reward for donations, it's not much but all we can give back to you as a thanks.</p>
+	<p>Donors will receive a icon of choice with every donation over 5€ behind their name.</p>
   </div>
 
-  <div class="col-xs-6">
   <h5>The qties</h5>
-  </div>
-
-  <div class="col-xs-6 donationlist" style="">
-  <table class="col-xs-6 table table-condensed" style="border-collapse: collapse;">
+  <div class="col-md-6 donationlist" style="">
+  <table class="table table-condensed" style="border-collapse: collapse;">
         <thead>
         <tr>
             <th>ID</th>
