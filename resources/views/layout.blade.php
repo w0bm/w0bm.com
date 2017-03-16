62d1e5abd60bee201a6284e5eb2d560ad4a38229
w0bm <admin@w0bm.com>
2017-03-16 22:30:42 +0100

    make spurdo -20px to the right again

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/bootstrap-tagsinput.css">
     <link rel="stylesheet" href="/css/video-js.min.css">
-    <link rel="stylesheet" href="/css/w0bmcustom.css?v=1.0.66">
+    <link rel="stylesheet" href="/css/w0bmcustom.css?v=1.0.67">
 <!-- Piwik -->
 <script type="text/javascript">
     var _paq = _paq || [];
