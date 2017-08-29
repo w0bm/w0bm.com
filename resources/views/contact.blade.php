8c6794a0942f0f4f26424e95dd68d35c9c5ec723
Abu Ottermann <w0bm@horsefucker.org>
2017-08-29 23:21:46 +0200

    Impressum ist schwul

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,6 +8,5 @@
 <h4>Please think about the following</h4>
 <p>If you want to have your content removed think about in which way w0bm.com could benefit you and your range, it's basically free advertisement for your content with loving and passionate people watching it, those people maybe heard the first time about you here on this website! So please think about all this before you send us complaints or DMCAs, we are not your enemy, we don't make money with your content and it's important to us, it's not right to make money with content you do not own or have the right to earn money with it and we do know that and we respect that! We don't want to be your enemy.</p>
 <h6 style="color: red;">Copyright related emails will be answered within 24 to 72 hours and infringing content will be removed as soon as possible, please provide enough information, that you are the rightful copyright holder.</h6>
-<a href="/impressum" style="font-size:10px">Impressum</a>
 @include('footer')
 @endsection
