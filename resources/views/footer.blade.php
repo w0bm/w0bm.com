db678aa12b87b9e03ce98c30f8f2420e2438c48f
Abu Ottermann <w0bm@horsefucker.org>
2017-12-05 10:38:32 +0100

    automatisches Jahr für copyright

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar-fixed-bottom">
   <div class="container futter">
     <a href="/rules">Rules</a> | <a href="/contact">Contact</a> | <a href="/privacy">Privacy</a> | <a href="/transparency">Transparency</a>
-<p>Inspired by <a href="http://z0r.de">z0r.de</a> | © 2015 – 2017 w0bm.com</p>
+<p>Inspired by <a href="http://z0r.de">z0r.de</a> | © 2015 – {{ date('Y') }} w0bm.com</p>
   </div>
 </nav>
