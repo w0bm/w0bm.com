12cfcc652b1d7d4238be1f5e54651ea5d600e581
Abu Ottermann <w0bm@horsefucker.org>
2017-12-26 20:06:20 +0100

    weihnachten aus

--- config/comments.php
+++ config/comments.php
@@ -6,12 +6,10 @@ return [
     'media.giphy.com',
     'files.nogf.club',
     'f0ck.me',
-    'f0ck.cf',
-    'f0ck.space',
     'files.catbox.moe',
     '68.media.tumblr.com',
+    '78.media.tumblr.com',
     'krautshare.net',
-    'a.safe.moe',
   ],
   'allowedImageFileExtensions' => [
     'jpg',
