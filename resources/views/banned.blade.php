0479d445b2cbf11db51c65c76213bd22937245b9
Abu Ottermann <sirx@firemail.cc>
2018-01-18 07:15:59 +0100

    neue bann seite

--- resources/views/banned.blade.php
+++ resources/views/banned.blade.php
@@ -1,15 +1,23 @@
 @extends('profilelayout')
 @section('content')
-<div class="panel pn-black">
-  <h1 id="b4nned">YOU ARE BANNED!</h1>
-  <video class="banvideo" src="https://b.w0bm.com/1515965864.webm" autoplay loop></video>
-    <div class="bannedomg">
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">YOU ARE BANNED!</h3>
+  </div>
+
+  <div class="panel-body">
+    <div style="border:0;" class="panel panel-default">
+      <div class="panel-body">
         @if($perm)
-            <span id="bantime">Silence you furry fool, you are finished here!</span><br>
+        <p>You ban is permanent and will <b>NOT</b> expire!</p>
         @else
-            <span id="bantime">Your ban will expire in {{ $user->banend->diffForHumans(null, true) }}</span><br>
+        <p>Reason: {{ $user->banreason }}</p>
+        <p>Your ban will expire in {{ $user->banend->diffForHumans(null, true) }}</p>
+        <img class="" src="/otter-ban.png">
         @endif
-            <span id="banreason">Reason: {{ $user->banreason }}</span><br>
+      </div>
     </div>
+    <p>If you think you were banned by accident or dindu nuffin to deserve the ban contact an administrator in the <a href="/irc">IRC</a></p>
+  </div>
 </div>
 @endsection
