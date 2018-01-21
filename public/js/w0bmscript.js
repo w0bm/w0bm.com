06ea8a04ea9b33a5c99f63a3f3ca3c4b2f2b93c6
jkhsjdhjs <leon.moeller@yahoo.de>
2018-01-21 13:26:28 +0000

    fix tag comma seperation on upload page

--- public/js/w0bmscript.js
+++ public/js/w0bmscript.js
@@ -334,7 +334,7 @@ $(function() {
 
     $('.delete-tag').on('click', tagDeleteHandler);
 
-    $('#tags, #filter').on('itemAdded', e => setTimeout(() => $(e.currentTarget).siblings('.bootstrap-tagsinput').children('input').val(''), 0));
+    $('#tags, #filter, #tags_upload').on('itemAdded', e => setTimeout(() => $(e.currentTarget).siblings('.bootstrap-tagsinput').children('input').val(''), 0));
 
     tagsinput.on('beforeItemAdd', e => {
         for(let tag of video.tags) {
