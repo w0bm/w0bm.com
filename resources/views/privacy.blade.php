db771ae17c02cbfe85af224c71417aa83744070f
Abu Ottermann <w0bm@horsefucker.org>
2018-01-23 06:48:33 +0000

    Removing Piwik (Motombo) analytics since our domain has been blacklisted and is being blocked by most clients now.

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,7 +1,6 @@
 @extends('profilelayout')
 @section('content')
 <h5>What do we log?</h5>
-<p>We use our own self-hosted Piwik instance to get some info on what's going on, down below you have the option to deactivate tracking if you don't want to be tracked.</p>
-<iframe style="border: 0; height: 200px; width: 600px;" src="https://observation.stasi.club/index.php?module=CoreAdminHome&action=optOut&language=en"></iframe>
+<p>Anything our Nginx logs within it's logs, however there are no user-account bound ip adresses.</p>
 @include('footer')
 @endsection
