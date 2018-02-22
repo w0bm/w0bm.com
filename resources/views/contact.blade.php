7c5baae33f70e3f22a015b81f308c5926a418eae
Abu Ottermann <w0bm@horsefucker.org>
2018-02-22 08:07:26 +0000

    veraltetes Protokoll abgeschoben

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,10 +3,8 @@
 <h4>Contact</h4>
 <img src="/traurig.gif" height="25%" width="25%" style="float: right;">
 <ul>
-<li>For contact/abuse/etc: <a href="mailto:w0bm@horsefucker.org">w0bm@horsefucker.org</a></li>
+<li>Contact: <a href="https://webirc.n0xy.net/?join=%23w0bm">#w0bm on n0xy.net</a> </li>
 </ul>
-<h4>Please think about the following</h4>
-<p>If you want to have your content removed think about in which way w0bm.com could benefit you and your range, it's basically free advertisement for your content with loving and passionate people watching it, those people maybe heard the first time about you here on this website! So please think about all this before you send us complaints or DMCAs, we are not your enemy, we don't make money with your content and it's important to us, it's not right to make money with content you do not own or have the right to earn money with it and we do know that and we respect that! We don't want to be your enemy.</p>
-<h6 style="color: red;">Copyright related emails will be answered within 24 to 72 hours and infringing content will be removed as soon as possible, please provide enough information, that you are the rightful copyright holder.</h6>
+<p>We don't give any support via email, fuck you if you want to communicate via email, we wont do it and you can simply fuck off or come to the IRC, it's your choice, btw I will only answer you in my cryptic German, I don't do english in serious biz, because hey I am not perfect in it, I hope you understand!</p>
 @include('footer')
 @endsection
