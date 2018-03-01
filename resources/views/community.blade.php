d3c83868198b1e3b90bb88d95d1703baeda122b8
Abu Ottermann <w0bm@horsefucker.org>
2018-03-01 02:13:16 +0000

    oooopsie community seite vergessen xD

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -1,19 +1,26 @@
 @extends('profilelayout')
 @section('content')
+ <div class="page-header">
+         <h3>w0bm.com Community</h3>
+ </div>
 
-<h5>w0bm.com Community</h5>
-
+<div class="box">
+<h5>Talk to your local internet nerds</h5>
 <p>You can choose between two communities, you can either join our IRC Server, or you can come to our Discord Server, it's up to you!</p>
+</div>
 
-<h5>IRC</h5>
+<div class="box">
 <img src="/irccat.gif" alt="irc cat" style="float: right; width: 25%; height: 25%;">
-<h4>irc.n0xy.net +6697 (ssl only) #w0bm</h4>
+<h5>IRC</h5>
+<h6>irc.n0xy.net +6697 (ssl only) #w0bm</h6>
 <p>Don't have a desktop  client? Why not join our Network via webirc? <a href="https://webirc.n0xy.net/?join=%23w0bm" target="about_blank">>>webirc.n0xy.net</a></p>
+<p>More information: <a href="https://n0xy.net">n0xy.net</a></p>
 
 <h5>Discord</h5>
 <iframe src="https://discordapp.com/widget?id=417311017912238090&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
 
 <p>Or as normal join link: <a href="https://discord.gg/wWcGVdp">https://discord.gg/wWcGVdp</a></p>
+</div>
 @include('footer')
 @endsection
 
