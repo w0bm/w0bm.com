7d326c5d9020d6d689b477ddbf26135e1aa21087
Abu Ottermann <w0bm@horsefucker.org>
2018-03-22 17:32:42 +0000

    hallo belst

---
+++ resources/views/tip.blade.php
@@ -0,0 +1,23 @@
+@extends('profilelayout')
+@section('content')
+<div class="page-header">
+	<h3>Donate</h3>
+</div>
+<div class="box">
+<img src="https://img.w0bm.com/u/vjhnwt.png" style="width: 5%; float:right; border-radius: 75%; transform: scalex(-1);">
+<h4>need money to serve memes :(</h4>
+<p>Hello good people of the internet!</p>
+<p>If you like this project we would be happy if you donate a small amount of money to fill our tip jar and help us to continue with this project!</p>
+<p>Our tip goal is not very high so we don't need much money, but it's still good to have some spare money in case something goes wrong.</p>
+<p>We need <strong>15€</strong> every year to pay for the domain.</p>
+<p>If you donate at least 5€ you can have a customized icon behind your name and you support this project and we will love you forever because I will never forget you!</p>
+<h4>Goal overview: <span style="color:#1FB2B0;">REACHED</span></h4>
+<div class="progress" style="width: 300px;">
+  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="15" style="width: 100%">
+  <span>101€</span>
+  </div>
+</div>
+<p>BTC: 1HbDdiQWAVs1zpjuAhrmBvdTjyrV84hdQ1</p>
+</div>
+@include('footer')
+@endsection
