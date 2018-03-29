bde723ed26d9d7051e8926d17b6caaceec88bef5
Abu Ottermann <w0bm@horsefucker.org>
2018-03-29 19:12:38 +0000

    Fick dich Kim :)

--- resources/views/transparency.blade.php
+++ resources/views/transparency.blade.php
@@ -7,7 +7,8 @@
 <div class="box">
 <h5>Takedown notices and other shit</h5>
 <ul>
-	<li><a href="/07072016-FSM.txt">07.07.2016 FSM Takedown Notice</a> (Wasn't even "real" childporn, just some random Germany bullshit... My advice is to tell your kids not to fuck some random guy from school in the changing room and make a video out of it, but hey #YOLO! </li>
+	<li><a href="/07072016-FSM.txt">07.07.2016 FSM Takedown Notice</a> (Wasn't even "real" childporn, just some random Germany bullshit... My advice is to tell your kids not to fuck some random guy from school in the changing room and make a video out of it, but hey #YOLO!)</li>
+	<li><a href="/kim.txt">Kim.txt</a> (Feinstes Mett vom Mettkönig bzgl. unserer Politik im internen. HTML is foll schwär!!!!!)</li>
 </ul>
 </div>
 @include('footer')
