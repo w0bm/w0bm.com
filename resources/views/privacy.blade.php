5e286a6dc53e940477e1f6d6e31a53c95533f3de
Abu Ottermann <w0bm@horsefucker.org>
2018-06-02 20:15:46 +0000

    updating privacy page

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -7,6 +7,13 @@
 <div class="box">
 <h5>What do we log?</h5>
 <p>Anything our Nginx logs within it's logs, however there are no user-account bound ip adresses.</p>
+<p>We value your privacy and will never give out any user data or sell user data (implying someone would really want to buy this shit anyways lol)</p>
+<p>We don't want to know who you are!</p>
+<h5>Why do we log?</h5>
+<p>Simply to prevent abuse.</p>
+<h5>³rd party links</h5>
+<p>There a some 3rd party linkings on this website, be careful with clicking them we don't know whats inside the box blah blah blah, don't be stupid and don't trust the internet.</p>
+<p>We also use a webfont from google fonts, if you don't like that go fuck yourself or block it with umatrix.</p>
 </div>
 @include('footer')
 @endsection
