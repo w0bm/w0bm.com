a63131278fb434dc7e9c21f05d20cb393dfa328d
jkhsjdhjs <leon.moeller@yahoo.de>
2018-06-03 00:54:15 +0000

    fix '3rd' on privacy page

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -11,7 +11,7 @@
 <p>We don't want to know who you are!</p>
 <h5>Why do we log?</h5>
 <p>Simply to prevent abuse.</p>
-<h5>³rd party links</h5>
+<h5>3<sup>rd</sup> party links</h5>
 <p>There a some 3rd party linkings on this website, be careful with clicking them we don't know whats inside the box blah blah blah, don't be stupid and don't trust the internet.</p>
 <p>We also use a webfont from google fonts, if you don't like that go fuck yourself or block it with umatrix.</p>
 </div>
