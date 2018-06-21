0af2a2c3a8661c01e751659274266b68db9a51b0
http <http@oc2.totally.rip>
2018-06-21 22:32:22 +0000

    HumanReadable.php

---
+++ app/Helpers/HumanReadable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helpers;
+
+class HumanReadable
+{
+    public static function bytesToHuman($bytes)
+    {
+        $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'];
+
+        for ($i = 0; $bytes > 1024; $i++) {
+            $bytes /= 1024;
+        }
+
+        return round($bytes, 2) . ' ' . $units[$i];
+    }
+}
