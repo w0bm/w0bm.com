b4614b8b7c6fabe25472dd14557fa19f3850ae8a
Abu Ottermann <w0bm@horsefucker.org>
2018-06-22 22:54:51 +0000

    mp4 filesize ist doof

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -65,8 +65,7 @@
 							<div class='downloadvid'>
 							<ul class='downloadlist'>
 							<li><a href='@if(env('APP_DEBUG')){{'/b'}}@else{{'//' . (substr($_SERVER['HTTP_HOST'], 0, 3) === 'v4.' ? 'v4.' : '') . 'b.w0bm.com'}}@endif{{ '/' . $video->file }}' download>WebM</a> <span class='filesize'>({{ HumanReadable::bytesToHuman($video->filesize()) }})</span></li>
-							{{--*/ $fapplesize = $video->fapplesize() /*--}}
-							<li><a href='//fapple.w0bm.com/{{str_replace('.webm','',$video->file)}}.mp4' download>MP4</a> <span class='filesize'>@if($fapplesize !== false)({{ HumanReadable::bytesToHuman($fapplesize) }})@endif</span></a>
+							<li><a href='//fapple.w0bm.com/{{str_replace('.webm','',$video->file)}}.mp4' download>MP4</a> <span class='filesize'>(N/A)</span></a>
 							</ul>
 							</div>
 							"
