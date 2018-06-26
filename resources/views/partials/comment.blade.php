a13bee830b998b4b1cd6b215bb8015c8083ee879
sirx <sirx@nogf.club>
2018-06-26 02:13:19 +0000

    comment time hover tooltip position top

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -2,7 +2,7 @@
     <div class="panel-body">
         @simplemd($comment->content)
     </div>
-    <div class="panel-footer">by <a href="/user/{{$comment->user->username}}">{!! $comment->user->displayName() !!}</a> <small><time class="timeago" data-toggle="tooltip" data-placement="right" datetime="{{$comment->created_at}}+0000" title="{{$comment->created_at}}+0000"></time></small>
+    <div class="panel-footer">by <a href="/user/{{$comment->user->username}}">{!! $comment->user->displayName() !!}</a> <small><time class="timeago" data-toggle="tooltip" data-placement="top" datetime="{{$comment->created_at}}+0000" title="{{$comment->created_at}}+0000"></time></small>
         @if($mod)
             @if($del)
                 <a href="#" onclick="restoreComment($(this))"><i style="color:green"; class="fa fa-refresh" aria-hidden="true"></i></a>
