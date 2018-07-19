6ae71e4275e8a39b84062ecc8c5590d9e7d9f843
sirx <sirx@nogf.club>
2018-07-19 19:58:22 +0000

    updating privacy policy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -5,8 +5,12 @@
  </div>
 
 <div class="box">
+<h5>What does Cloudflare log?</h5>
+<p>Probably anything that is possible to log.</p>
+<h5>What can we access from the Cloudflare logs?</h5>
+<p>Only basic stuff like the amount of visitors and where they are from, no ips and also no chance to get the raw log files.</p>
 <h5>What do we log?</h5>
-<p>Anything our Nginx logs within it's logs, however there are no user-account bound ip adresses.</p>
+<p>Anything our Nginx logs (if we are not using cloudflare) within it's logs, however there are no user-account bound ip adresses.</p>
 <p>Our server creates the log in this format: <br />
 <code>x.x.x.x (IP address) - - [13/Mar/37:13:37:00 -9999] (time) "GET /1337 HTTP/1.1 (method, destination & protocol) " 200 3158 (http resonse code)"-" "USER AGENT"</code></p>
 <p>We value your privacy and will never give out any user data or sell user data (implying someone would really want to buy this shit anyways lol)</p>
@@ -16,6 +20,8 @@
 <h5>3<sup>rd</sup> party links</h5>
 <p>There a some 3rd party linkings on this website, be careful with clicking them we don't know whats inside the box blah blah blah, don't be stupid and don't trust the internet.</p>
 <p>We also use a webfont from google fonts, if you don't like that go fuck yourself or block it with umatrix.</p>
+<h5>Cookies</h5>
+<p>Yes, we use cookies if you don't like that don't come to our site! (or don't accept them in the first place)</p>
 </div>
 @include('footer')
 @endsection
