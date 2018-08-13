d8cd00fed2ea53921acee431d11613640d8c7237
noxy <sirx@nogf.club>
2018-08-13 05:55:52 +0200

    pimp my reg #2

--- resources/views/partials/loginmodal.blade.php
+++ resources/views/partials/loginmodal.blade.php
@@ -5,17 +5,18 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="filterModalTitle">Login</h4>
             </div>
-            <div class="modal-body">
+            
+            <div class="login_wrapper">
+            	<div class="col-md-9">
+					<form action="{{action('UserController@login')}}" method="post" class="navbar-form">
+						{!! csrf_field() !!}
+                	     <input type="text" name="identifier" placeholder="Username" class="form-control usernamelogin"><br>
+                	     <input type="password" name="password" placeholder="Password" class="form-control passwordlogin"><br>
+                	     <button type="submit" class="btn btn-primary">Login</button> <input id="rememberme" type="checkbox" name="remember"> <span id="rember">Remember me?</span>
+					</form>
+				</div>
+	   		</div>
 
-	<form action="{{action('UserController@login')}}" method="post" class="navbar-form navbar-right">
-	{!! csrf_field() !!}
-		<div class="loginmodaldiv">
-                        <input type="text" name="identifier" placeholder="Username" class="form-control usernamelogin"><br>
-                        <input type="password" name="password" placeholder="Password" class="form-control passwordlogin"><br>
-                        <button type="submit" class="btn btn-primary">Login</button> <input id="rememberme" type="checkbox" name="remember"> <span id="rember">Remember me?</span>
-		</div>
-	</form>
-	   </div>
 	    	<div class="modal-footer">
 	    		<p>No w0bm account yet? <a href="#" data-toggle="modal" data-target="#registermodal" data-dismiss="modal">Register now!</a></p>
         	</div>
--- resources/views/partials/registermodal.blade.php
+++ resources/views/partials/registermodal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="registermodal" tabindex="-1" role="dialog" aria-labelledby="Login">
+<div class="modal fade" id="registermodal" tabindex="-1" role="dialog" aria-labelledby="Register">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -6,42 +6,52 @@
                 <h4 class="modal-title" id="filterModalTitle">Register</h4>
             </div>
             
-            <div class="modal-body">
+            <div class="advantages">
+              <p>Registering on w0bm has some advantages...</p>
+              <p>With a account you can comment on WebMs, favor WebMs, upload WebMs, tag videos, filter out unwanted tags and watch NSFW WebMs!</p>
+            </div>
+
+            <div class="reg_wrapper">
 	   		        <div class="register">
-        <div class="col-md-8">
-        <form class="form-horizontal" method="post" action="{{action('UserController@store')}}">
-            {!! csrf_field() !!}
-            <div class="form-group">
-                <div class="">
-                    {!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
+                  <div class="col-md-10">
+                    <form class="form-horizontal" method="post" action="{{action('UserController@store')}}">
+                      {!! csrf_field() !!}
+                        <div class="form-group">
+                          <div class="reg_username">
+                            {!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username']) !!}
+                          </div>
+                        </div>
+                
+                        <div class="form-group">
+                          <div class="reg_password">
+                            {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+                          </div>
+                        </div>
+              
+                        <div class="form-group">
+                          <div class="reg_password">
+                            {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Password Confirmation']) !!}
+                          </div>
+                        </div>
+            
+                        <div class="form-group">
+                          <div class="recaptcha">
+                            {!! Recaptcha::render() !!}
+                          </div>
+                        </div>
+                      
+                        <div class="form-group terms">
+                          <div class="verify_rules">
+                            <p><input type="checkbox" required name="terms"> I am at least 18 years or older and I have read and understand the <a href="/rules">Rules</a></p>
+                          </div>
+                        
+                          <div class="submit_registration">
+                            <button type="submit" class="btn btn-primary">Register</button>
+                          </div>
+                        </div>
+                    </form>
+                  </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="">
-                    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="">
-                    {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Password Confirmation']) !!}
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="">
-                    {!! Recaptcha::render() !!}
-                </div>
-      </div>
-      <div class="form-group terms">
-    <div style="">
-    <p><input type="checkbox" required name="terms"> I am at least 18 years or older and I have read and understand the <a href="/rules">Rules</a></p>
-    </div>
-    <div class="">
-                    <button type="submit" class="btn btn-primary">Register</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    </div>
             </div>
 
             <div class="modal-footer">
