9660495c50c03c375c746fdd8ed08ba01df4d12d
sirx <sirx@nogf.club>
2018-08-16 20:41:40 +0000

    vergessen xd

--- resources/views/partials/commentform.blade.php
+++ resources/views/partials/commentform.blade.php
@@ -12,10 +12,11 @@
 	<a style="font-size: 14px; color: white;" class="spoiler" href="javascript:;" onclick="formatText ('spoiler');">[spoiler]</a>
         <div tabindex="0" class="onclick-menu">
             <ul class="onclick-menu-content">
-                <li>Protips:</li>
+		<li>Protips:</li>
                 <li>Do not stack them like this <code>[rb][reich]I am funny ;)))[/reich][/rb]</code></li>
                 <li>This will look like shit and also it wont work.</li>
-                <li>Hit Enter 2x to make a normal paragraph. (Yes we are retarded)</li>
+		<li>Hit Enter 2x to make a normal paragraph. (Yes we are retarded)</li>
+		<li>Link to a video on w0bm.com is easy but 99% of you do it wrong! <br> <code>Blah Blah Blah /1337</code> this will not resolve the full link, just paste the full link in there like this: <code>Blah Blah Blah https://w0bm.com/1337</code> and this will result in the following: <p>Blah Blah Blah <a href="https://w0bm.com/1337">/1337</a></li>
                 <li>See <a href="/about#format">/about</a> for additional formatting options</li>
             </ul>
         </div>
